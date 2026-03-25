48b2cc959797643528dcac5552068e1977099dc3
issei <proteinebimayoduu@gmail.com>
2026-03-26 04:35:08 +0900

    プロフィール画像の導入・削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :avatar
+  
   validates :account_name, presence: true, uniqueness: true
 end
