19e87b149f844c5d0355187e7ceade5a1d4e5af8
issei <proteinebimayoduu@gmail.com>
2026-03-27 12:02:08 +0900

    プロフィール画像のリサイズ・プレビュー・削除機能を実装

---
+++ app/javascript/controllers/avatar_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["input", "preview", "removeFlag"]
+
+  select() {
+    this.inputTarget.click()
+  }
+
+  preview(event) {
+    const file = event.target.files[0]
+    if (!file) return
+
+    this.removeFlagTarget.value = "0"
+
+    const reader = new FileReader()
+
+    reader.onload = (e) => {
+      this.previewTarget.src = e.target.result
+    }
+
+    reader.readAsDataURL(file)
+  }
+
+  reset() {
+    this.previewTarget.src = this.previewTarget.dataset.defaultAvatarUrl
+    this.inputTarget.value = ""
+    this.removeFlagTarget.value = "1"
+  }
+}
