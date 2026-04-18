ec89292641f6aaa0bc4ffc55260f1a84497c2e9c
issei <proteinebimayoduu@gmail.com>
2026-04-18 20:15:04 +0900

    フォロー機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
+  get "users/show"
   devise_for :users
   root to: "home#index"
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   get "up" => "rails/health#show", as: :rails_health_check
-
-  resource :profile, only: [:show, :update]
   
   resources :posts, only: [:index, :new, :create, :show] do
     resource :like, only: [:create, :destroy]
     resources :comments, only: [:create]
   end
+
+  resources :users, only: [:show, :update]
+  resources :relationships, only: [:create, :destroy]
 end
