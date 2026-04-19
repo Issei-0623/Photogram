22af53b664a4d7b2bed036729e64025ce2e8181a
issei <proteinebimayoduu@gmail.com>
2026-04-19 12:53:15 +0900

    タイムライン機能を実装する（フォロー中＋人気投稿）

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,14 @@ class Post < ApplicationRecord
 
     has_many :likes, dependent: :destroy
     has_many :comments, dependent: :destroy
+
+    scope :recent, -> { order(created_at: :desc) }
+
+    scope :created_within_24h, -> { where(created_at: 24.hours.ago..Time.current) }
+
+    scope :popular, -> {
+      joins(:likes)
+        .group("posts.id")
+        .order("COUNT(likes.id) DESC")
+    }
 end
